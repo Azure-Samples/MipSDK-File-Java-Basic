c8ab6cdd22fbfef807f93da197132a7f784a1876
Tom Moser <tommos@microsoft.com>
2021-03-11 14:44:25 -0800

    Update license and readme.

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+---
+page_type: sample
+languages:
+- java
+products:
+- azure
+description: "This sample application demonstrates using the Microsoft Information Protection SDK Java wrapper to label and read a label from a file."
+urlFragment: mipsdk-filesdk-java-sample
+---
 # MIP SDK Java Wrapper Sample
 
 This sample application demonstrates a very basic MIP SDK Java wrapper sample. It'll demonstrate how to create the project, add dependencies, and get to a place where the app can run on both Windows and Ubuntu. 
--- mipsdksample/src/main/java/com/microsoft/mipsdksample/App.java
+++ mipsdksample/src/main/java/com/microsoft/mipsdksample/App.java
@@ -1,3 +1,29 @@
+/*
+*
+* Copyright (c) Microsoft Corporation.
+* All rights reserved.
+*
+* This code is licensed under the MIT License.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files(the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions :
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*
+*/
 package com.microsoft.mipsdksample;
 
 import java.io.BufferedReader;
@@ -19,7 +45,7 @@ public class App
         ApplicationInfo appInfo = new  ApplicationInfo();
         FileOptions options = new FileOptions();
 
-        appInfo.setApplicationId("46d42b3f-c156-46b7-9f7b-55da52c07b8c");
+        appInfo.setApplicationId("YOUR CLIENT ID");
         appInfo.setApplicationName("MIP SDK Java Sample");
         appInfo.setApplicationVersion("1.8.97");
 
