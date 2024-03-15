44322749f15329c94d67190679c9798c49905483
Yangchen Zhang <yangczhang@microsoft.com>
2024-03-15 11:49:58 -0700

    update auth delegate for single tenant app

--- mip-filesdk-java-sample/src/main/java/com/microsoft/mipsdksample/AuthDelegateImpl.java
+++ mip-filesdk-java-sample/src/main/java/com/microsoft/mipsdksample/AuthDelegateImpl.java
@@ -62,6 +62,12 @@ public class AuthDelegateImpl implements IAuthDelegate {
         }
 
         AUTHORITY = authority;
+
+        if (AUTHORITY.toLowerCase().contains("common")) {
+            URI authorityUri = new URI(AUTHORITY);
+            AUTHORITY = String.format("https://%s/%s", authorityUri.getHost(), tenant);
+        }
+        
         String token = "";
         try {
             token = acquireTokenInteractive().accessToken();
